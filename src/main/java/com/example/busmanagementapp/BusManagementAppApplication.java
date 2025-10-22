00c2291003012ae7848eee355fe702eb4e6caa7d
Bianca Sava <biancasava07@gmail.com>
2025-10-22 10:47:26 +0300

    Project Created

---
+++ src/main/java/com/example/busmanagementapp/BusManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.busmanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusManagementAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusManagementAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/busmanagementapp/BusManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.busmanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusManagementAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

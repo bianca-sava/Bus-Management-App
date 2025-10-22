2e39771e4db40a3c45e90548630e9d96532f4ba8
Simon Raul-Alexandru <raulsimonalexas@yahoo.com>
2025-10-22 11:32:25 +0300

    Staff + Driver + TripManager implemented

---
+++ src/main/java/com/example/busmanagementapp/modell/DutyAssignment.java
@@ -0,0 +1,4 @@
+package com.example.busmanagementapp.modell;
+
+public class DutyAssignment {
+}
---
+++ src/main/java/com/example/busmanagementapp/modell/Staff.java
@@ -0,0 +1,20 @@
+package com.example.busmanagementapp.modell;
+
+public abstract class Staff {
+    protected String id;
+    protected String name;
+
+    public Staff(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    /// Getters
+    public abstract String getId();
+    public abstract String getName();
+
+    /// Setters
+    public abstract void setId(String id);
+    public abstract void setName(String name);
+
+}
